234ca24e4fd61794c53d56019a0e6d5f9eec88af
Kharamot <kameronharamoto@Kamerons-Mac.local>
2017-01-30 20:53:32 -0800

    Finished app

--- ViewController.swift
+++ ViewController.swift
@@ -8,35 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var textField: UITextField!
-    var commentArray: Array<Any>
+    var commentArray: Array<Comment> = []
+    var commentToAdd: String = ""
     
     @IBAction func addButtonPressed(_ sender: Any) {
         if (textField.text == ""){
             
         }
         else{
-            var newComment = Comment(newComment: textField.text!)
-            commentArray.append(newComment: Comment.self)
+            let c = Comment(newComment: textField.text!)
+            commentArray.append(c)
+            textField.text = ""
         }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
     }
     
     @IBAction func printAllPressed(_ sender: Any) {
         //Print all to console by date.
+        for item in commentArray {
+            print(item.comment + " " + item.date)
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
